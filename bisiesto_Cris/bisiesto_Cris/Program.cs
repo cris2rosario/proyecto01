5052f5a2b152460fb2eaa0d772c6fbfc8e64fc60
cris2rosario <rosariocrisleidy3@gmail.com>
2025-01-23 21:11:23 -0400

    Add files via upload

---
+++ bisiesto_Cris/bisiesto_Cris/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bisiesto_Cris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el año, para revisar si es bisiesto:  ");
+            string entrada = Console.ReadLine();
+
+            if (int.TryParse(entrada, out int año))
+            {
+                if (EsAñoBisiesto(año))
+                {
+                    Console.WriteLine($"El año {año} es bisiesto.");
+                }
+                else
+                {
+                    Console.WriteLine($"El año {año} no es bisiesto.");
+                }
+            }
+            else
+            {
+               Console.WriteLine("Favor introduce un numero entero.");
+            }
+        }
+
+        static bool EsAñoBisiesto(int año)
+        {
+           return (año % 4 == 0 && (año % 100 != 0 || año % 400 == 0));
+        }
+
+    }
+    }
+
